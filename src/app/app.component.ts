a19c622fd2052f8ba9cd5ce256ca75a7c49c7a0f
Murat Çelik <muratcelik11@gmail.com>
2021-08-08 02:22:01 +0300

    via

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'primeng-sandbox';
+  title!: String;
+
+  upper() {
+    this.title = this.title.toUpperCase();
+  }
 }
